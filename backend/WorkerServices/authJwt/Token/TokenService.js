289dfb8d3712b4b946b0709630a3e04e07cf0b5d
DrewsAbuse <drewsdrews1999@gmail.com>
2021-05-17 20:06:06 +0200

    Token `rip`Class => ()->{}

--- backend/WorkerServices/authJwt/Token/TokenService.js
+++ backend/WorkerServices/authJwt/Token/TokenService.js
@@ -12,32 +12,28 @@ function generateAccessToken(id, roles, username) {
     console.log(error)
   }
 }
-class JwtService {
-  constructor(jwt, secret) {}
 
-  async create(req, res) {
-    try {
-      const { id, role, username } = req.body
-      console.log(id, role, username)
-      const token = generateAccessToken(id, role, username)
+const create = async (req, res) => {
+  try {
+    const { id, role, username } = req.body
+    console.log(id, role, username)
+    const token = generateAccessToken(id, role, username)
 
-      res.status(201).json({ token: token })
-    } catch (error) {
-      res.status(400).json({ verify: false, error })
-      console.log(error)
-    }
-  }
-  async verify(req, res) {
-    const token = req.headers.authorization.split(' ')[1]
-
-    try {
-      const DecryptedJwt = await jwt.verify(token, secret)
-      res.status(200).json({ verify: true, DecryptedJwt: DecryptedJwt })
-    } catch (error) {
-      res.status(401).json({ verify: false })
-      console.log('token LOG', error)
-    }
+    res.status(201).json({ token: token })
+  } catch (error) {
+    res.status(400).json({ verify: false, error })
+    console.log(error)
   }
 }
+const verify = async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1]
 
-module.exports = new JwtService(jwt, secret)
+  try {
+    const DecryptedJwt = await jwt.verify(token, secret)
+    res.status(200).json({ verify: true, DecryptedJwt: DecryptedJwt })
+  } catch (error) {
+    res.status(401).json({ verify: false })
+    console.log('token LOG', error)
+  }
+}
+module.exports = { create, verify }
